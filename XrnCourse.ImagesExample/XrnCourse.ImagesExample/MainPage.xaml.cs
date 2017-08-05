57c5949c1906a6512cc9c3e3acf9749459cc6f4c
sigged <sderdeyn@gmail.com>
2017-08-05 20:36:02 +0200

    added embedded image to PCL

--- XrnCourse.ImagesExample/XrnCourse.ImagesExample/MainPage.xaml.cs
+++ XrnCourse.ImagesExample/XrnCourse.ImagesExample/MainPage.xaml.cs
@@ -12,6 +12,15 @@ namespace XrnCourse.ImagesExample
         public MainPage()
         {
             InitializeComponent();
+
+            var embeddedImage = new Image { Aspect = Aspect.AspectFill };
+            //get the image by ResourceID
+            embeddedImage.Source = ImageSource.FromResource("XrnCourse.ImagesExample.images.glider.png");
+            //configure absolute layout 
+            AbsoluteLayout.SetLayoutBounds(embeddedImage, new Rectangle(.5, .5, 512, 109));
+            AbsoluteLayout.SetLayoutFlags(embeddedImage, AbsoluteLayoutFlags.PositionProportional);
+            //add image to layout
+            absLayout.Children.Add(embeddedImage);
         }
     }
 }
