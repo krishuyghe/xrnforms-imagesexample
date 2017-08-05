f91316eb8e6db03260f4e3fd56812fbd82115564
sigged <sderdeyn@gmail.com>
2017-08-05 20:55:29 +0200

    Added ImageResourceExtension to support displaying embedded images in XAML

---
+++ XrnCourse.ImagesExample/XrnCourse.ImagesExample/Extensions/ImageResourceExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XrnCourse.ImagesExample.Extensions
+{
+    [ContentProperty("Source")]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+            //get the ImageSource based on the given ResourceId 
+            var imageSource = ImageSource.FromResource(Source);
+
+            return imageSource;
+        }
+    }
+}
+//code courtesy of Xamarin
